4b60600508c072ed8b84e38c2aa7ae68c7f8c808
Dušan Borovčanin <dusan.borovcanin@mainflux.com>
2020-01-29 14:31:53 +0100

    Add License Agent client
    Signed-off-by: Dušan Borovčanin <dusan.borovcanin@mainflux.com>

--- client.go
+++ client.go
@@ -3,8 +3,11 @@
 
 package license
 
+// Handler handles validation response.
+type Handler func(error)
+
 // Client represents licensing service client API specification.
 type Client interface {
 	// Validate validates service against provided license.
-	Validate(svcName string) error
+	Validate(svcName string)
 }
